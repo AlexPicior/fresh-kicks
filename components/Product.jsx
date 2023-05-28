bf71a1605ac01141b027b06cb1ca6d54448fd7dc
AlX <alexpicior@yahoo.com>
2023-05-28 23:27:05 +0300

    commit19

--- components/Product.jsx
+++ components/Product.jsx
@@ -18,7 +18,7 @@ const Product = ({product}) => {
       <Link href={`/product/${slug}`}>
         <img className='xs:w-full lg:w-[15vw] brightness-100 hover:brightness-50 transition-all duration-500' src={images && images[0]} alt="" />
       </Link>
-      {isInFavsItems(product) ? <button className='z-20 relative bottom-6 xs:left-[78%] ss:left-[83%] lg:left-[88%]' onClick={() => onRemoveFavs(product)}><AiFillHeart className=' h-6 w-6 max-w-fit text-rose-600'/></button> : <button className='z-20 relative bottom-6 xs:left-[78%] ss:left-[83%] lg:left-[88%]' onClick={() => onAddFavs(product)}><AiOutlineHeart className='h-6 w-6 max-w-fit'/></button>}
+      {isInFavsItems(product) ? <button className='z-20 relative bottom-6 xs:left-[78%] ss:left-[83%] lg:left-[88%] xxl:left-[90%]' onClick={() => onRemoveFavs(product)}><AiFillHeart className=' h-6 w-6 max-w-fit text-rose-600'/></button> : <button className='z-20 relative bottom-6 xs:left-[78%] ss:left-[83%] lg:left-[88%] xxl:left-[90%]' onClick={() => onAddFavs(product)}><AiOutlineHeart className='h-6 w-6 max-w-fit'/></button>}
 
         <div className='flex lg:px-6 relative bottom-3 flex-col'>
           <span className='font-poppins line-clamp-3 xs:text-xs sss:text-sm lg:text-base text-center font-medium'>{name}</span>
